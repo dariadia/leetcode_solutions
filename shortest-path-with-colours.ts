49dc844762788b77e26d5b55d92106397a81d27c
Daria V. Diachkova <daria.diachkova@mail.ru>
2023-02-12 09:26:20 +0100

    Update shortest-path-with-colours.ts

--- shortest-path-with-colours.ts
+++ shortest-path-with-colours.ts
@@ -8,9 +8,75 @@
 // Return an array answer of length n, where each answer[x] is 
 // the length of the shortest path from node 0 to node x such that the edge colors alternate along the path, or -1 if such a path does not exist.
 
-// Algo: 
-// We are given a directed graph with n nodes from 0 to n-1 having red and blue edges.
-// Our task is to return an array of integers answer where answer[x] 
-// is the length of the shortest path from node 0 to node x 
-// such that the edge colors alternate along the path, or -1 if such a path does not exist.
-// 
+// Algo: a directed graph, unweighted = BFS
+// Create a list (graph) of n-length
+// Fill out so that contains a list of pairs such that graph[node] has the neighbors with colors specified
+// Create a answer array with the value -1 where answer[i] is the answer for the i-th node
+// Create a "visited" Object
+// Create a queue 
+// While queue has items: loop on. Fill out result array, get nextVal from graph, change colors
+
+
+type Color = 'r' | 'b';
+type Graph = {
+  [key: string]: {
+    [key in Color]: number[];
+  };
+};
+type Visited = {
+  [key in Color]: Set<number>;
+};
+
+function shortestAlternatingPaths(
+  n: number,
+  red_edges: number[][],
+  blue_edges: number[][]
+): number[] {
+  const graph: Graph = {};
+  // all nodes neighbors in the graph
+  for (let i = 0; i < n; i++) graph[i] = { r: [], b: [] };
+  red_edges.forEach(([i, j]) => graph[i].r.push(j));
+  blue_edges.forEach(([i, j]) => graph[i].b.push(j));
+
+  const res: number[] = Array(n).fill(-1);
+  const visited: Visited = {
+    r: new Set<number>(),
+    b: new Set<number>(),
+  };
+
+  const queue: [number, Color][] = [
+    [0, 'r'],
+    [0, 'b'],
+  ];
+
+  let len = 0;
+
+  while (queue.length) {
+    const j = queue.length;
+
+    for (let i = 0; i < j; i++) {
+      const [currValue, currColor] = queue.shift()!;
+
+      if (res[currValue] === -1) {
+        res[currValue] = len;
+      }
+
+      const nextValues = graph[currValue][currColor];
+      const nextColor = currColor === 'b' ? 'r' : 'b';
+
+      nextValues.forEach((nextValue) => {
+        if (!visited[nextColor].has(nextValue)) {
+          visited[nextColor].add(nextValue);
+          queue.push([nextValue, nextColor]);
+        }
+      });
+    }
+    len++;
+  }
+
+  return res;
+}
+
+// Time complexity: O(n+e) - where n is the number of nodes and e is the total number of blue and red edges
+// One queue operation is O(1), one can be pushed twice, so O(n). an edge can be operated on twice so O(e)
+// Space: O(n+e)
